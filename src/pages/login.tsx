7f82b742a0d8324e092057d5c606304ec00c24c0
joaovitor-pires <joao.2023014293@alunocesul.com>
2025-07-29 22:42:12 -0300

    Iniciando a tela de Login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,16 @@
+import { Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import loginImage from "../../public/assets/login-image.gif";
+
 export default function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+  return (
+    <Flex w="100vw" h="100vh">
+      <Flex w="50%" bg="#2C73EB" align="center" justify="center">
+        <Image w={500} h={500} src={loginImage.src} />
+      </Flex>
+      <VStack w="50%" bg="green.400" align="start">
+        <Heading as="h1">Account Login</Heading>
+        <Text>If you already</Text>
+      </VStack>
+    </Flex>
+  )
 }
