ac5ab89581d3e7042e3147e01959716d9c661236
joaovitor-pires <joao.2023014293@alunocesul.com>
2025-08-05 21:37:06 -0300

    Continuando tela de Login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,8 @@
-import { Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { Button, Link as ChakraLink, Field, Flex, Heading, HStack, Image, Input, Stack, Text, VStack } from "@chakra-ui/react";
+import NextLink from "next/link";
 import loginImage from "../../public/assets/login-image.gif";
+import { Checkbox } from "@/components/ui/checkbox";
+import { PasswordInput } from "@/components/ui/password-input";
 
 export default function Login() {
   return (
@@ -7,9 +10,43 @@ export default function Login() {
       <Flex w="50%" bg="#2C73EB" align="center" justify="center">
         <Image w={500} h={500} src={loginImage.src} />
       </Flex>
-      <VStack w="50%" bg="green.400" align="start">
-        <Heading as="h1">Account Login</Heading>
-        <Text>If you already</Text>
+      <VStack w="50%" justify="center">
+        <Stack>
+          <Heading as="h1" color="black" fontSize="3xl" fontWeight="bold">Entrar</Heading>
+
+          <Text color="gray.400" fontSize="lg" fontWeight="normal">Se você já é membro, você pode fazer login com seu endereço de e-mail e senha.</Text>
+
+          <VStack align="flex-start" gap={6} mt={10}>
+            <Field.Root>
+              <Field.Label color="gray.500" fontSize="md">
+                Email
+              </Field.Label>
+              <Input type="email" h={16} colorPalette="blue" borderRadius="md" color="black"/>
+            </Field.Root>
+
+            <Field.Root>
+              <Field.Label color="gray.500" fontSize="md">
+                Senha
+              </Field.Label>
+              <PasswordInput h={16} colorPalette="blue" borderRadius="md" color="black"/>
+            </Field.Root>
+
+            <Checkbox colorPalette="blue" color="gray.500" fontSize="md" fontWeight="medium">
+              Lembre-me
+            </Checkbox>
+
+            <Button w="full" h={16} colorPalette="blue" borderRadius="md" fontSize="md" fontWeight="medium">
+              Entrar
+            </Button>
+          </VStack>
+
+          <HStack justify="center" gap={1} mt={10}>
+            <Text color="gray.500" fontSize="md" fontWeight="medium">Não possui uma conta?</Text>
+            <ChakraLink color="blue.500" asChild>
+              <NextLink href="/sign-up">Clique aqui!</NextLink>
+            </ChakraLink>
+          </HStack>
+        </Stack>
       </VStack>
     </Flex>
   )
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "@chakra-ui/react";
-import { tokens } from "@chakra-ui/react/theme";
 
 export const themeConfig = defineConfig({
   globalCss: {
     "html, body": {
-      backgroundColor: "gray.50",
+      backgroundColor: "white",
     }
   },
     theme: {
